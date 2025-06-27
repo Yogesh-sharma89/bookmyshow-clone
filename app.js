08a665e8d7e7528c37a784fbfe7f15a84472740e
Yogesh sharma <nitya.upmanyu@gmail.com>
2025-06-27 10:18:57 +0530

    loader animation added

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var swiper = new Swiper(".topSwiper", {
   spaceBetween: 30,
   centeredSlides: false,
-  loop:true,
-  speed:700,
+  loop: true,
+  speed: 700,
   autoplay: {
     delay: 2100,
 
@@ -31,13 +31,13 @@ for (let nextbtn of nextbtns) {
   nextbtn.addEventListener("click", (e) => {
     const wrapper = e.target.closest('.wrapper');
     // console.log(wrapperSection);
-    const slider1=wrapper.querySelector('.card-slide-1');
-    const slider2=wrapper.querySelector('.card-slide-2');
-  // console.log(slider1,slider2);
+    const slider1 = wrapper.querySelector('.card-slide-1');
+    const slider2 = wrapper.querySelector('.card-slide-2');
+    // console.log(slider1,slider2);
     slider1.classList.add('hato');
     slider2.classList.add('upar');
-    const prevbtn=wrapper.querySelector('.prev-button');
-    console.log(prevbtn,nextbtn);
+    const prevbtn = wrapper.querySelector('.prev-button');
+    console.log(prevbtn, nextbtn);
     nextbtn.style.opacity = 0;
     prevbtn.style.opacity = 1;
   })
@@ -47,14 +47,14 @@ for (let nextbtn of nextbtns) {
 for (let prevBtn of prevBtns) {
   prevBtn.addEventListener("click", (e) => {
 
-const wrapper = e.target.closest('.wrapper');
+    const wrapper = e.target.closest('.wrapper');
     // console.log(wrapperSection);
-    const slider1=wrapper.querySelector('.card-slide-1');
-    const slider2=wrapper.querySelector('.card-slide-2');
-   console.log(slider1,slider2); 
+    const slider1 = wrapper.querySelector('.card-slide-1');
+    const slider2 = wrapper.querySelector('.card-slide-2');
+    console.log(slider1, slider2);
     slider1.classList.remove('hato');
     slider2.classList.remove('upar');
-    const nextBtn=wrapper.querySelector('.next-button');
+    const nextBtn = wrapper.querySelector('.next-button');
     prevBtn.style.opacity = 0;
     nextBtn.style.opacity = 1;
   })
@@ -72,6 +72,16 @@ function raf(time) {
 requestAnimationFrame(raf);
 
 
+
+window.addEventListener('DOMContentLoaded', (e) => {
+  let loader = document.querySelector('.loader');
+  console.log(loader);
+  setTimeout((e) => {
+    loader.classList.add('niklo');
+      window.scrollTo(0, 0);
+    document.documentElement.scrollTop = 0;
+  }, 3455);
+})
 
 
 
